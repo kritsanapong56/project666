2f36e318ffafdf86f6f081ef3413bea8ca924f2e
kritsanapong56 <kritsanapong115599@gmail.com>
2024-02-09 12:37:08 +0700

    day

--- lib/ui/medicine/frequency.dart
+++ lib/ui/medicine/frequency.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_safealert/tool/color.dart';
 import 'package:flutter_alarm_safealert/ui/medicine/AddTimeAlertMedicine.dart';
 import 'package:flutter_alarm_safealert/ui/medicine/TypeMedicine.dart';
+import 'package:flutter_rounded_date_picker/flutter_rounded_date_picker.dart';
+import 'package:intl/intl.dart';
 
 class frequency extends StatefulWidget {
   const frequency({super.key});
@@ -12,86 +16,360 @@ class frequency extends StatefulWidget {
 }
 
 class _frequencyState extends State<frequency> {
+  var _stxtHBD = "";
+  TextEditingController txtHBD = TextEditingController();
   int selectedNumber = 1;
   String halo = "0";
+  void dateHBD(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+    Timer(const Duration(milliseconds: 400), () {
+      int yearCurrent = DateTime.now().year + 1;
+      showRoundedDatePicker(
+        borderRadius: 25,
+        styleDatePicker: MaterialRoundedDatePickerStyle(
+          textStyleDayHeader: const TextStyle(
+              fontFamily: 'SukhumvitSet-Bold',
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              color: AppColors.color_grey),
+          textStyleDayOnCalendar: const TextStyle(
+              fontFamily: 'SukhumvitSet-Bold',
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              color: AppColors.color_grey),
+          paddingMonthHeader: EdgeInsets.all(10),
+          sizeArrow: 40,
+          colorArrowPrevious: AppColors.colorMain,
+          colorArrowNext: AppColors.colorMain,
+          textStyleMonthYearHeader: const TextStyle(
+              fontFamily: 'SukhumvitSet-Bold',
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+              color: Colors.black),
+          textStyleButtonPositive: const TextStyle(
+              fontFamily: 'SukhumvitSet-Bold',
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: AppColors.color_grey),
+          textStyleButtonNegative: const TextStyle(
+              fontFamily: 'SukhumvitSet-Bold',
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: AppColors.color_grey),
+        ),
+        theme: ThemeData(
+          primaryColor: AppColors.colorMain,
+          hintColor: AppColors.colorMain,
+          textTheme: const TextTheme(
+            caption: TextStyle(
+                fontFamily: 'SukhumvitSet-Bold',
+                fontWeight: FontWeight.w500,
+                color: AppColors.color_grey),
+          ),
+        ),
+        // era: EraMode.BUDDHIST_YEAR,
+        context: context,
+        height: 330,
+        fontFamily: 'SukhumvitSet-Bold',
+        // locale: const Locale("th"),
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1850),
+        lastDate: DateTime(yearCurrent),
+      ).then((date) {
+        setState(() {
+          _stxtHBD = DateFormat('yyyy-MM-dd').format(date!);
+          var strDay = DateFormat('dd/MM/yyyy').format(date!);
+          txtHBD.text = strDay;
+
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: AppColors.colorMain,
-          title: new Text("ความถี่การทานยา",style: TextStyle(
-            fontSize: 25,color: Colors.black,
-              fontFamily: 'SukhumvitSet-Bold'),),
-          leading: IconButton(
-            icon: ImageIcon(
-                AssetImage("assets/images/arrow_left.png"),
-                  size:40,
-                  color: Colors.black,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+        title: const Text(
+          "ความถี่การทานยา",
+          style: TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+              fontFamily: 'SukhumvitSet-Bold'),
+        ),
+        leading: IconButton(
+          icon: const ImageIcon(
+            AssetImage("assets/images/arrow_left.png"),
+            size: 40,
+            color: Colors.black,
           ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20,40,20,20),
-            child: Center(
-              child: Column(
-                children: [
-                  Text(
-                    'ความถี่การทานยา',
-                    style: TextStyle(fontSize: 30),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+        child: Center(
+          child: Column(
+            children: [
+              const Text(
+                'ความถี่การทานยา',
+                style: TextStyle(fontSize: 25,
+                color: Colors.black,
+                fontFamily:'SukhumvitSet-Bold' ),
+              ),
+              SizedBox(height: 20),
+              DropdownButton<int>(
+                value: selectedNumber,
+                items: const [
+                  DropdownMenuItem(
+                    value: 1,
+                    child: Text(
+                      "ประจำวัน",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontFamily: 'SukhumvitSet-Bold'),
+                    ),
                   ),
-                  SizedBox(height: 20),
-                  DropdownButton<int>(
-                    value: selectedNumber,
-                    items: [
-                      DropdownMenuItem(
-                        child: Text("ประจำวัน",style: TextStyle(
-                fontSize: 25,color: Colors.black,
-                  fontFamily: 'SukhumvitSet-Bold'),),
-                        value: 1,
-                        
-                      ),
-                      DropdownMenuItem(
-                        child: Text("วันของสัปดาห์",style: TextStyle(
-                fontSize: 25,color: Colors.black,
-                  fontFamily: 'SukhumvitSet-Bold'),),
-                        value: 2,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("ช่วงวัน",style: TextStyle(
-                fontSize: 25,color: Colors.black,
-                  fontFamily: 'SukhumvitSet-Bold'),),
-                        value: 3,
-                      ),
-                    
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        selectedNumber = value!;
-                        
-                      });
-                    },
+                  DropdownMenuItem(
+                    value: 2,
+                    child: Text(
+                      "วันของสัปดาห์",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontFamily: 'SukhumvitSet-Bold'),
+                    ),
                   ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AddTimeAlertMedicine(halo)),
-                      );
-                      print('เลือกข้อมูล: $selectedNumber');
-                    },
-                    child: Text('หน้าถัดไป'),
+                  DropdownMenuItem(
+                    value: 3,
+                    child: Text(
+                      "ช่วงวัน",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontFamily: 'SukhumvitSet-Bold'),
+                    ),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedNumber = value!;
+                  });
+                },
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: 10,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin:
+                              const EdgeInsets.only(top: 20, left: 10, right: 10),
+                          padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                          width: double.maxFinite,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment
+                                .start, // Align text to the start
+                            children: [
+                              const Text(
+                                "เริ่มต้น",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontFamily: 'SukhumvitSet-Medium',
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(
+                                  height:
+                                      10), // Adjust the spacing between "เริ่มต้น" and TextField
+                              TextField(
+                                onTap: () {
+                                  dateHBD(context);
+                                },
+                                controller: txtHBD,
+                                readOnly: true,
+                                style: const TextStyle(
+                                  fontSize: 25.0,
+                                  fontFamily: 'SukhumvitSet-Bold',
+                                  color: Colors.black,
+                                ),
+                                decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10.0,
+                                    horizontal: 15,
+                                  ),
+                                  border: const OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(20.0),
+                                    ),
+                                    borderSide: BorderSide(
+                                        width: 0, style: BorderStyle.none),
+                                  ),
+                                  filled: true,
+                                  hintText: "วันที่เริ่มทาน",
+                                  hintStyle: TextStyle(
+                                    fontFamily: 'SukhumvitSet-Medium',
+                                    color: Colors.grey[800],
+                                  ),
+                                  fillColor: AppColors.bgColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddTimeAlertMedicine(halo)),
+                  );
+                  print('เลือกข้อมูล: $selectedNumber');
+                },
+                child: Text('หน้าถัดไป'),
+              ),
+            ],
           ),
-      );
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 60,
+        margin: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                margin: EdgeInsets.only(right: 5),
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+                    ),
+                    shape: MaterialStateProperty.resolveWith(
+                        (states) => RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            )),
+                    backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => AppColors.colorMain),
+                    elevation: MaterialStateProperty.resolveWith<double>(
+                      (Set<MaterialState> states) {
+                        return 2.0;
+                      },
+                    ),
+                  ),
+                  onPressed: () {
+                    // Navigator.push(context,
+                    //     CupertinoPageRoute(builder: (context) {
+                    //       return MainMenu();
+                    //     }));
+                    Navigator.pop(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: const Text(
+                          "ย้อนกลับ",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontFamily: 'SukhumvitSet-Bold'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                margin: const EdgeInsets.only(left: 5),
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.only(
+                          left: 5, right: 5, top: 10, bottom: 10),
+                    ),
+                    shape: MaterialStateProperty.resolveWith(
+                        (states) => RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            )),
+                    backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => AppColors.colorMain),
+                    elevation: MaterialStateProperty.resolveWith<double>(
+                      (Set<MaterialState> states) {
+                        return 2.0;
+                      },
+                    ),
+                  ),
+                  // onPressed: () {
+                  //   if(txtMedicine.text.isNotEmpty && _selectedIndex > -1) {
+                  //     AppUrl.objAddItemMedicine.nameMedicine = txtMedicine.text.toString();
+                  //     AppUrl.objAddItemMedicine.timeTakeId = _selectedTimeTakeId;
+                  //     _pushPagetest(context,false);
+                  //    }else {
+                  //     _openPopupInvalidate(context,"กรุณากรอกข้อมูลให้ครบถ้วน");
+                  //   }
+                  // },
 
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AddTimeAlertMedicine(halo)),
+                    );
+                    print('เลือกข้อมูล: $selectedNumber');
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: const Text(
+                          "ถัดไป",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontFamily: 'SukhumvitSet-Bold'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget buildNumberButton(int number) {
